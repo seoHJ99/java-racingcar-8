712d04e0f2e08e3bb87a8a9ae58d35264c5fbde5
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2025-10-25 00:38:01 +0900

    feat: 추가. 자동차 객체 생성시 이름 유효성 검증하는 기능
    자동차 이름은 별도의 유효성 검증이 필요하므로 Valued Object 로 뺀다.

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,9 @@
+package racingcar.domain;
+
+public class Car {
+    public CarName name;
+
+    public Car(CarName name){
+        this.name = name;
+    }
+
---
+++ src/main/java/racingcar/domain/CarName.java
@@ -0,0 +1,23 @@
+package racingcar.domain;
+
+public class CarName {
+    private final String value;
+
+    public CarName(String value){
+        value = value.trim();
+
+        if(value.isEmpty()){
+            throw new IllegalArgumentException("빈값은 이름이 될 수 없습니다.");
+        }
+
+        if(value.length() >5){
+            throw new IllegalArgumentException("자동차 이름은 5자 이하로 제한됩니다.");
+        }
+
+        this.value = value;
+    }
+
+    public String getName(){
+        return value;
+    }
+}
