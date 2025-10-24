f1f617c0b56e9dd691a6b6f8d3b8fd47392b9cd8
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2025-10-25 01:12:13 +0900

    feat: 생성된 자동차 정보를 저장/조회하는 기능
    생성된 자동차를 저장/ 조회한다. 이때 Map으로 저장하면 순서가 항상 같으리란 보장이 없음으로 List로 저장한다.

---
+++ src/main/java/racingcar/domain/CarRepository.java
@@ -0,0 +1,21 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarRepository {
+
+    // 순서 보장을 위해 리스트로 저장
+    public static final List<Car> carRepository = new ArrayList<>();
+
+    public static void saveCar(Car car){
+        carRepository.add(car);
+    }
+
+    public static Car findByName(String name){
+        return carRepository.stream()
+                .filter(car -> car.getName().equals(name))
+                .findFirst()
+                .get();
+    }
+}
