a72f67043da7e574b99d520afb43cdd20e5ff4ea
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2025-10-25 18:44:17 +0900

    feat: 사용자로부터 입력값을 받기 위해 질문을 출력하는 기능

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,12 +1,22 @@
 package racingcar.view;
 
-import racingcar.domain.Car;
-import racingcar.domain.CarRepository;
-import racingcar.domain.LocationRepository;
+import racingcar.domain.car.Car;
+import racingcar.domain.repository.LocationRepository;
 
 import java.util.List;
 
 public class OutputView {
+
+    private static final String REQUEST_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String REQUEST_COUNT = "시도할 횟수는 몇 회인가요?";
+
+    public static void requestNames(){
+        System.out.println(REQUEST_NAMES);
+    }
+
+    public static void requestCount(){
+        System.out.println(REQUEST_COUNT);
+    }
 
     public void outputOnceTryResult(List<Car> cars) {
         StringBuilder sb = new StringBuilder();
