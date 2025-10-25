fc15b4c92bcb91d9e12a01b48cd4f470a1f68b8b
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2025-10-26 00:17:14 +0900

    feat: 최종 우승자 출력하는 기능

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -4,21 +4,30 @@ import racingcar.domain.car.Car;
 import racingcar.domain.repository.LocationRepository;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class OutputView {
 
     private static final String REQUEST_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String REQUEST_COUNT = "시도할 횟수는 몇 회인가요?";
+    private static final String FINAL_WINNER = "최종 우승자 : ";
 
-    public static void requestNames(){
+    public static void requestNames() {
         System.out.println(REQUEST_NAMES);
     }
 
-    public static void requestCount(){
+    public static void requestCount() {
         System.out.println(REQUEST_COUNT);
     }
 
-    public void outputOnceTryResult(List<Car> cars) {
+    public static void outputFinalResult(List<Car> winners) {
+        String names = winners.stream()
+                .map(car -> car.getName())
+                .collect(Collectors.joining(", "));
+        System.out.println(FINAL_WINNER + names);
+    }
+
+    public static void outputOnceTryResult(List<Car> cars) {
         StringBuilder sb = new StringBuilder();
         for (Car car : cars) {
             sb.append(car.getName());
@@ -29,7 +38,7 @@ public class OutputView {
         System.out.println(sb);
     }
 
-    private String outputLocation(Car car) {
+    private static String outputLocation(Car car) {
         StringBuilder sb = new StringBuilder();
         int location = LocationRepository.getLocation(car);
         for (int i = 0; i < location; i++) {
