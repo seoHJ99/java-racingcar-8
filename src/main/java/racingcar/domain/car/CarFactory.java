537da90beb18deedd55b4620e948edafa3bd3aec
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2025-10-26 16:24:30 +0900

    fix: 쉼표만 이름으로 들어왔을때 이름 빈값으로 인식하여 예외 발생

--- src/main/java/racingcar/domain/car/CarFactory.java
+++ src/main/java/racingcar/domain/car/CarFactory.java
@@ -1,5 +1,7 @@
 package racingcar.domain.car;
 
+import racingcar.exception.EmptyNameException;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -7,8 +9,13 @@ import java.util.stream.Collectors;
 public class CarFactory {
 
     public List<Car> createCars(String input) {
-        return Arrays.stream(input.split(","))
+        List<Car> cars = Arrays.stream(input.split(","))
                 .map(Car::new)
                 .collect(Collectors.toList());
+
+        if(cars.size()==0)
+            throw new EmptyNameException();
+        
+        return cars;
     }
 }
