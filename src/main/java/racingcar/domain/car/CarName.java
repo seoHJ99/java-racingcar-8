2babca6e6f2012a60a889c2b8daaa4e3aadcc912
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2025-10-26 16:18:34 +0900

    refactor: 자동차 이름 오류도 커스텀 오류로 변경

--- src/main/java/racingcar/domain/car/CarName.java
+++ src/main/java/racingcar/domain/car/CarName.java
@@ -1,13 +1,16 @@
 package racingcar.domain.car;
 
+import racingcar.exception.EmptyNameException;
+import racingcar.exception.Over5LetterNameException;
+
 public class CarName {
     private final String value;
 
     public CarName(String value) {
         value = value.trim();
 
-        if (value.isEmpty()) throw new IllegalArgumentException("빈값은 이름이 될 수 없습니다.");
-        if (value.length() > 5) throw new IllegalArgumentException("자동차 이름은 5자 이하로 제한됩니다.");
+        if (value.isEmpty()) throw new EmptyNameException();
+        if (value.length() > 5) throw new Over5LetterNameException();
 
         this.value = value;
     }
